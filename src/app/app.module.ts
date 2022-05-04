8406b2d1fd0cfb5a2031986bb7e97c93c38843c1
Timo Scarabel <scarabelt@gmail.com>
2022-05-04 13:33:49 +0200

    Added firebase to app
    Added firebase modules and connected app to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAnalytics(() => getAnalytics()), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ScreenTrackingService,UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'my-gps-tracker-ae891',
+    appId: '1:404303533124:web:4cf51e97e66f0b5efe9811',
+    storageBucket: 'my-gps-tracker-ae891.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDonxPuZaYGhmiLSctRbBhu8fRupqlq0Y8',
+    authDomain: 'my-gps-tracker-ae891.firebaseapp.com',
+    messagingSenderId: '404303533124',
+    measurementId: 'G-CBPD7705MX',
+  },
   production: false
 };
 
