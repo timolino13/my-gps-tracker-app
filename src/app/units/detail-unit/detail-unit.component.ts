ab96d2125b71c1f58582aa6ee795ff9fe56da7a6
Timo Scarabel <scarabelt@gmail.com>
2022-05-09 14:47:23 +0200

    Created unit detail

--- src/app/units/detail-unit/detail-unit.component.ts
+++ src/app/units/detail-unit/detail-unit.component.ts
@@ -1,14 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {UnitsService} from '../units.service';
+import {Subscription, timer} from 'rxjs';
+import {Unit} from '../../models/unit';
+import {FirebaseDevice} from '../../models/firebase-device';
 
 @Component({
-  selector: 'app-detail-unit',
-  templateUrl: './detail-unit.component.html',
-  styleUrls: ['./detail-unit.component.scss'],
+	selector: 'app-detail-unit',
+	templateUrl: './detail-unit.component.html',
+	styleUrls: ['./detail-unit.component.scss'],
 })
-export class DetailUnitComponent implements OnInit {
+export class DetailUnitComponent implements OnInit, OnDestroy {
+	unit: Unit = null;
+	devices: FirebaseDevice[] = null;
 
-  constructor() { }
+	selectedDeviceId: number;
 
-  ngOnInit() {}
+	private unitSubscription: Subscription;
 
+	constructor(private readonly route: ActivatedRoute, private readonly unitsService: UnitsService) {
+	}
+
+	ngOnInit() {
+		const unitId = this.route.snapshot.paramMap.get('unitId');
+
+		this.unitSubscription = timer(0, 10000).subscribe(() => {
+			this.getUnit(unitId);
+		});
+
+		this.getDevicesList();
+	}
+
+	getUnit(unitId: string) {
+		this.unitsService.getUnitById(parseInt(unitId, 10)).subscribe(async value => {
+				this.unit = await value;
+				console.log('Unit', this.unit);
+				if (this.unit.devices.length > 0) {
+					this.selectedDeviceId = this.unit.devices[0].id;
+				}
+			}
+		);
+	}
+
+	getDevicesList() {
+		return this.unitsService.getAllDevices$().subscribe(devices => {
+			console.log('Devices', devices);
+			this.devices = devices;
+		});
+	}
+
+	unsubscribe() {
+		if (this.unitSubscription) {
+			this.unitSubscription.unsubscribe();
+			console.log('UnitTimer unsubscribed');
+		}
+	}
+
+	ngOnDestroy() {
+		this.unsubscribe();
+	}
+
+	changeDevice() {
+
+	}
 }
